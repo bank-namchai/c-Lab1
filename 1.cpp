af7a482279cc0a77b8b92e9704a224fe7852af1d
Namchai Hangkasee <s6604062620158@email.kmutnb.ac.th>
2024-11-28 18:10:21 +0700

    เวอร์ชั่น 3

--- 1.cpp
+++ 1.cpp
@@ -2,13 +2,24 @@
 
 int main (){
 	
-	int num1=100;
-	int num2=500;
+	int num1;
+	int num2;
+	printf("n1:");
+	scanf("%d",&num1);
+		printf("n2:");
+	scanf("%d",&num2);
+	
+	
 	
 	if (num1>num2){
 		
 		printf(" num1 is greater than num2");
+		//diff of num1 and num2
+		printf("\n%d",(num1-num2));
 	}	else {
 		printf("num2 is greater than num1");
+		//diff of num2 and num1
+		printf("\n%d",(num2-num1));
+		
 	}
 }
