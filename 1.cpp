8b27d2d223d9653a7e24cc1d9b09670ede37b964
Namchai Hangkasee <s6604062620158@email.kmutnb.ac.th>
2024-11-28 17:49:19 +0700

    เก็บไฟล์

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+int main (){
+	
+	int num1=100;
+	int num2=500;
+	
+	if (num1>num2){
+		
+		printf(" num1 is greater than num2");
+	}
+	
+	if (num2>num1){
+		printf("num2 is greater than num1");
+	}
+}
